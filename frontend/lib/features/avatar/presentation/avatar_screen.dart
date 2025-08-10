8c0e6c58b4b4d3d84311a12c973c21ee5afae37c
Sara Fojt <sara.fojt167@gmail.com>
2025-08-10 17:01:19 +0200

    [fix:avatar_widget] Fix displaying avatar widget and emitting avatar state

--- frontend/lib/features/avatar/presentation/avatar_screen.dart
+++ frontend/lib/features/avatar/presentation/avatar_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/core/constants/app_theme.dart';
 import 'package:frontend/core/services/avatar_service.dart';
 import 'package:frontend/features/authentication/data/user_model.dart';
 import 'package:frontend/features/authentication/logic/user_storage.dart';
+import 'package:frontend/features/avatar/data/avatar_cubit.dart';
 import 'package:frontend/features/avatar/presentation/avatar_tabs.dart';
 import 'package:frontend/widgets/avatar.dart';
 import 'package:frontend/widgets/button.dart';
@@ -27,6 +29,9 @@ class AvatarScreen extends StatelessWidget {
                     child: Text("Error while loading streak: ${snapshot.error}"))
             );
           } else {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              context.read<AvatarCubit>().loadAvatar();
+            });
             return Scaffold(
               backgroundColor: ColorConstants.background,
               appBar: CustomAppBar(streak: snapshot.data!.streak),
--- frontend/lib/widgets/avatar.dart
+++ frontend/lib/widgets/avatar.dart
@@ -3,105 +3,118 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/core/constants/app_theme.dart';
 import 'package:frontend/core/constants/avatar_storage.dart';
 import 'package:frontend/features/avatar/data/avatar_cubit.dart';
+import 'package:frontend/features/avatar/data/avatar_state.dart';
 
 class AvatarWidget extends StatelessWidget {
   const AvatarWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final avatar = context.watch<AvatarCubit>().state;
+    context.read<AvatarCubit>().clearAvatar();
+    return BlocBuilder<AvatarCubit, AvatarState?>(
+        builder: (context, avatar) {
+          if (avatar == null) {
+            return Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                color: ColorConstants.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
 
-    if (avatar == null) {
-      return Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          color: ColorConstants.white,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: const Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
-    return Container(
-      width: 300,
-      height: 300,
-      decoration: BoxDecoration(
-        color: ColorConstants.white,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Center(
-        child: SizedBox(
-          width: 300,
-          height: 300,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Transform.scale(
-                scale: 8,
-                child: Image.network(
-                  'https://${AvatarStorage.storageName}.blob.core.windows.net/base/${avatar.skinColor}.png',
-                  filterQuality: FilterQuality.none,
-                ),
-              ),
-              Transform.scale(
-                scale: 8,
-                child: Image.network(
-                  'https://${AvatarStorage.storageName}.blob.core.windows.net/eyes/${avatar.eyesColor}.png',
-                  filterQuality: FilterQuality.none,
-                ),
-              ),
-              Transform.scale(
-                scale: 8,
-                child: Image.network(
-                  'https://${AvatarStorage.storageName}.blob.core.windows.net/bottom-clothes/${avatar.bottomClothes}-${avatar.bottomClothesColor}.png',
-                  filterQuality: FilterQuality.none,
-                ),
-              ),
-              Transform.scale(
-                scale: 8,
-                child: Image.network(
-                  'https://${AvatarStorage.storageName}.blob.core.windows.net/top-clothes/${avatar.topClothes}-${avatar.topClothesColor}.png',
-                  filterQuality: FilterQuality.none,
-                ),
-              ),
-              Transform.scale(
-                scale: 8,
-                child: Image.network(
-                  'https://${AvatarStorage.storageName}.blob.core.windows.net/hair/${avatar.hair}-${avatar.hairColor}.png',
-                  filterQuality: FilterQuality.none,
-                ),
-              ),
-              if (avatar.blush != "0")
+          return Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: ColorConstants.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
                 Transform.scale(
                   scale: 8,
                   child: Image.network(
-                    'https://${AvatarStorage.storageName}.blob.core.windows.net/blush/${avatar.blush}.png',
+                    'https://${AvatarStorage
+                        .storageName}.blob.core.windows.net/base/${avatar
+                        .skinColor}.png',
                     filterQuality: FilterQuality.none,
                   ),
                 ),
-              if (avatar.lipstick != "0")
                 Transform.scale(
                   scale: 8,
                   child: Image.network(
-                    'https://${AvatarStorage.storageName}.blob.core.windows.net/lipstick/${avatar.lipstick}.png',
+                    'https://${AvatarStorage
+                        .storageName}.blob.core.windows.net/eyes/${avatar
+                        .eyesColor}.png',
                     filterQuality: FilterQuality.none,
                   ),
                 ),
-              if (avatar.beard != "0")
                 Transform.scale(
                   scale: 8,
                   child: Image.network(
-                    'https://${AvatarStorage.storageName}.blob.core.windows.net/beard/${avatar.beard}.png',
+                    'https://${AvatarStorage
+                        .storageName}.blob.core.windows.net/bottom-clothes/${avatar
+                        .bottomClothes}-${avatar.bottomClothesColor}.png',
                     filterQuality: FilterQuality.none,
                   ),
                 ),
-            ],
-          ),
-        ),
-      ),
+                Transform.scale(
+                  scale: 8,
+                  child: Image.network(
+                    'https://${AvatarStorage
+                        .storageName}.blob.core.windows.net/top-clothes/${avatar
+                        .topClothes}-${avatar.topClothesColor}.png',
+                    filterQuality: FilterQuality.none,
+                  ),
+                ),
+                Transform.scale(
+                  scale: 8,
+                  child: Image.network(
+                    'https://${AvatarStorage
+                        .storageName}.blob.core.windows.net/hair/${avatar
+                        .hair}-${avatar.hairColor}.png',
+                    filterQuality: FilterQuality.none,
+                  ),
+                ),
+                if (avatar.blush != "0")
+                  Transform.scale(
+                    scale: 8,
+                    child: Image.network(
+                      'https://${AvatarStorage
+                          .storageName}.blob.core.windows.net/blush/${avatar
+                          .blush}.png',
+                      filterQuality: FilterQuality.none,
+                    ),
+                  ),
+                if (avatar.lipstick != "0")
+                  Transform.scale(
+                    scale: 8,
+                    child: Image.network(
+                      'https://${AvatarStorage
+                          .storageName}.blob.core.windows.net/lipstick/${avatar
+                          .lipstick}.png',
+                      filterQuality: FilterQuality.none,
+                    ),
+                  ),
+                if (avatar.beard != "0")
+                  Transform.scale(
+                    scale: 8,
+                    child: Image.network(
+                      'https://${AvatarStorage
+                          .storageName}.blob.core.windows.net/beard/${avatar
+                          .beard}.png',
+                      filterQuality: FilterQuality.none,
+                    ),
+                  ),
+              ],
+            ),
+          );
+        }
     );
   }
 }
